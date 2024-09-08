55d6622e10676647c24edab19b272186c8b918f6
Praveen Vijayan <praveenv.vijayan@gmail.com>
2024-09-08 16:13:27 +0400

    export description field removed

--- app/(authentication)/(prelogin)/login/page.tsx
+++ app/(authentication)/(prelogin)/login/page.tsx
@@ -4,9 +4,6 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-export const description =
-  "A login page with two columns. The first column has the login form with email and password. There's a Forgot your passwork link and a link to sign up if you do not have an account. The second column has a cover image.";
-
 export default function Login() {
   return (
     <div className="flex items-center justify-center py-12">
