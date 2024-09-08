9821508fc1dc74135035f5571a661588cd4ac938
Praveen Vijayan <praveenv.vijayan@gmail.com>
2024-09-08 16:11:05 +0400

    Updated dashboard discription

--- app/(authentication)/(dashboard)/dashboard/page.tsx
+++ app/(authentication)/(dashboard)/dashboard/page.tsx
@@ -32,9 +32,6 @@ import {
 import { Input } from "@/components/ui/input";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
-export const description =
-  "A products dashboard with a sidebar navigation and a main content area. The dashboard has a header with a search input and a user menu. The sidebar has a logo, navigation links, and a card with a call to action. The main content area shows an empty state with a call to action.";
-
 export default function Dashboard() {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
