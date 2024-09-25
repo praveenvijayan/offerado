b530a507e5041bf19822a461d2529c9f044ff3f6
Praveen Vijayan <praveenv.vijayan@gmail.com>
2024-09-25 20:53:14 +0400

    CORS origin localhost issue sorted

--- services/product-services.ts
+++ services/product-services.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import type { Product } from "@prisma/client";
 
 // API endpoint for products
-const API_URL = "http://localhost:3000/api/products";
+const API_URL = "/api/products";
 
 // Fetch all products
 const fetchProducts = async (): Promise<Product[]> => {
